7d1f5959ef325543171618ec18bf6e62e1dc8fb2
awaiswaris945 <awaiswaris945@gmail.com>
2018-03-01 10:07:10 +0100

    realisation de nouvel exe

---
+++ ex1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Hello</title>
+</head>
+<body>
+    
+    
+<?php
+echo("Hello world !");
+?>
+<br>    
+<a href="/tp1/ex1.php?message=Hello">Bonjour</a>
+<br>    
+<a href="/tp1/ex1.php?message=Hello">cava</a>
+<br>    
+<a href="/tp1/ex1.php?message=Hello">Bienvenue les enfants</a>
+<br>
+</body>
+</html>
---
+++ ex2.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Hello</title>
+</head>
+<body>
+    
+    
+<?php
+echo("Hello world !");
+?>
+<br>    
+<a href="/tp1/ex1.php?message=Hello">Bonjour</a>
+<br>    
+<a href="/tp1/ex1.php?message=Hello">cava</a>
+<br>    
+<a href="/tp1/ex1.php?message=Hello">Bienvenue les enfants</a>
+<br>
+    
+<div style="font-size: 10px;font-color:#FF0000">Message de taille 10px en rouge</div>
+    
+<form method="GET">
+    <label for="size">Size : </label>
+    <input type="number" value="" name="size" id="size">
+    <input type="submit" value="Valider">
+</form>
+    
+</body>
+</html>
